d6d5333e99b5ffc9bcfde3bd483b83341ce5f228
BlackBox-Ops <edukasitime@gmail.com>
2025-08-18 17:41:32 +0700

    memperbaiki load model dari SVM ke Logreg

--- src/test_new_predict.py
+++ src/test_new_predict.py
@@ -3,7 +3,7 @@ import pandas as pd # type: ignore
 import json
 import os
 
-model = joblib.load(os.path.join("../models", "svc_best_pipeline.joblib"))
+model = joblib.load(os.path.join("../models", "logisticregression_best_pipeline.joblib"))
 
 with open("../data/raw/sample_test_data.json", "r") as f:
     df_new = pd.DataFrame(json.load(f))
